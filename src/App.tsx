b6ed8d4c927ff25901d40736a7d5f1de3db4ee17
Dario Šehović <dario.sehovic@icloud.com>
2022-06-23 14:35:51 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Login from './pages';
+
 import './styles';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
